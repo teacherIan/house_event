629f7263b7b169326679fafbd744d96f5082651d
teacherIan <ian_malloy@ssis.asia>
2023-06-05 21:31:17 +0800

    bug fix

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,22 @@
   import pearl from './assets/pearl.jpeg';
   import Card from './lib/Card.svelte';
   import Pixi from './lib/Pixi.svelte';
+
+  if (window.localStorage.getItem('amber') === null) {
+    window.localStorage.setItem('amber', '0');
+  }
+
+  if (window.localStorage.getItem('ruby') === null) {
+    window.localStorage.setItem('ruby', '0');
+  }
+
+  if (window.localStorage.getItem('sapphire') === null) {
+    window.localStorage.setItem('sapphire', '0');
+  }
+
+  if (window.localStorage.getItem('pearl') === null) {
+    window.localStorage.setItem('pearl', '0');
+  }
 </script>
 
 <div class="container">
